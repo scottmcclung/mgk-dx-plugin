0991993c4e479ba1873955440e3444b7073cc130
Scott McClung <scott@nerdmagik.com>
2021-06-14 17:18:34 -0700

    Adding flag for custom objects only

--- src/commands/mgk/schema/export.ts
+++ src/commands/mgk/schema/export.ts
@@ -1,4 +1,4 @@
-import {flags, SfdxCommand} from '@salesforce/command';
+import {flags, FlagsConfig, SfdxCommand} from '@salesforce/command';
 import {Messages} from '@salesforce/core';
 import {AnyJson} from '@salesforce/ts-types';
 import CvsObjectScribe from '../../../shared/cvsObjectScribe';
@@ -19,7 +19,7 @@ export default class MgkSchemaExport extends SfdxCommand {
     `$ sfdx mgk:schema:export -sobject Account,Case,Opportuntiy -format xls --targetpath ./dir/example-filename.xls --targetusername myOrg@example.com`
   ];
 
-  public static readonly flagsConfig = {
+  public static readonly flagsConfig: FlagsConfig = {
     format: flags.enum({
       char: 'f',
       description: messages.getMessage('schema.export.flags.format'),
@@ -37,6 +37,10 @@ export default class MgkSchemaExport extends SfdxCommand {
       char: 's',
       description: messages.getMessage('schema.export.flags.sobject'),
       required: false
+    }),
+    customobjectsonly: flags.boolean({
+      description: messages.getMessage('schema.export.flags.customObjectsOnly'),
+      required: false
     })
   };
 
@@ -45,7 +49,8 @@ export default class MgkSchemaExport extends SfdxCommand {
   public async run(): Promise<AnyJson> {
     const metadataExport = new MetadataExport({
       org: this.org,
-      sobjects: this.flags.sobjects
+      sobjects: this.flags.sobjects,
+      customObjectsOnly: this.flags.customobjectsonly
     });
     const metadata = await metadataExport.getExport();
     this.outputToFile(metadata);
@@ -62,11 +67,4 @@ export default class MgkSchemaExport extends SfdxCommand {
       default:
     }
   }
-
-  // options to implement
-  // allow user to specify sobject groups as "all", "custom", "standard", "customized"
-  // "customized" = all custom objects + any standard objects with custom fields.
-
-  // add first tab to the data that lists the objects and object attributes
-  // or add object attributes to the top of each object tab
 }
