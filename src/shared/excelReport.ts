ca7c5d43f8daff74fbbeed5656f6a1f07b2d2aea
Scott McClung <scott@nerdmagik.com>
2021-06-30 23:33:39 -0700

    Shorten long object names to fit in worksheet name limits

--- src/shared/excelReport.ts
+++ src/shared/excelReport.ts
@@ -1,14 +1,13 @@
 import {Workbook} from "exceljs";
-// const Excel = require('exceljs');
 import {headerMap, summaryHeaderMap} from './exportSettings';
 
 const headers = () => {
     return headerMap.map(column => {
         return {
-            key: column.fieldDataKey,
+            key:    column.fieldDataKey,
             header: column.columnTitle,
-            width: column.width,
-            style: column.style
+            width:  column.width,
+            style:  column.style
         };
     });
 };
@@ -16,33 +15,33 @@ const headers = () => {
 const sobjectHeaders = () => {
     return summaryHeaderMap.map(column => {
         return {
-            key: column.fieldDataKey,
+            key:    column.fieldDataKey,
             header: column.columnTitle,
-            width: column.width,
-            style: column.style
+            width:  column.width,
+            style:  column.style
         };
     });
-}
+};
 
 const getWorksheet = (workbook: Workbook, name: string, headers: object[]) => {
     const worksheet = workbook.addWorksheet(name);
     worksheet.columns = headers;
     worksheet.autoFilter = {
         from: 'A1',
-        to: 'I1',
+        to:   'I1',
     };
     worksheet.views = [
         {state: 'frozen', ySplit: 1}
     ];
     return worksheet;
-}
+};
 
 const generateObjectSummaryWorksheet = (workbook: Workbook, metadata: Map<string, object>) => {
     const worksheet = getWorksheet(workbook, 'SObject', sobjectHeaders());
     for (const [key, sobject] of metadata.entries()) {
         worksheet.addRow({...sobject});
     }
-}
+};
 
 const generateObjectWorksheets = (workbook: Workbook, worksheetName: string, worksheetData: object) => {
     if (worksheetData.fields) {
@@ -51,15 +50,34 @@ const generateObjectWorksheets = (workbook: Workbook, worksheetName: string, wor
             worksheet.addRow({...field});
         }
     }
-}
+};
+
+/**
+ * Excel has a 31 char limit on worksheet names
+ * This generates a new map of the metadata with the keys shortened to 31 char.
+ * To avoid duplicate worksheet names we're appending an index number to the
+ * shortened name.
+ */
+const generateMetadataWithShortObjectNames = (metadata: Map<string, object>) => {
+    return new Map(
+        Array
+            .from(metadata)
+            .map((value, index) => {
+                if (value[0].length <= 31) return value;
+                let length = 31 - index.toString().length
+                value[0] = value[0].substr(0, length) + index.toString()
+                return value;
+            })
+    );
+};
 
 export default class ExcelReport {
     public static write(filePath: string, metadata: Map<string, object>) {
         const workbook = new Workbook();
         generateObjectSummaryWorksheet(workbook, metadata);
-        for (const [key, sobject] of metadata.entries()) {
+        for (const [key, sobject] of generateMetadataWithShortObjectNames(metadata).entries()) {
             generateObjectWorksheets(workbook, key, sobject);
         }
         workbook.xlsx.writeFile(`${filePath}.xlsx`);
     }
-}
+};
